c2ef91b7238aa23a6d7887247fae21757554b70b
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-07 19:34:48 -0800

    Implement LeafTask class

--- src/main/model/LeafTask.java
+++ src/main/model/LeafTask.java
@@ -3,51 +3,52 @@ package model;
 // Represents a task with no subtasks of its own
 // Inter-class structure based on D4 (Composite Pattern) Lecture Labs
 public class LeafTask extends Task {
+    private Date dueDate;
+    private int weight;
+    private boolean isCompleted;
 
     // EFFECTS: creates a leaf task (task with no subtasks) with the given name, description, due date, and weight
     public LeafTask(String name, String description, Date dueDate, int weight) {
         super(name, description);
-        //TODO Auto-generated constructor stub
+        this.dueDate = dueDate;
+        this.weight = weight;
     }
 
     // MODIFIES: this
     // EFFECTS: sets the due date to the given date
     public void setDueDate(Date newDate) {
-
+        this.dueDate = newDate;
     }
 
     // REQUIRES: weight > 0
     // MODIFIES: this
     // EFFECTS: sets the task weight to the given weight
     public void setWeight(int newWeight) {
-
+        this.weight = newWeight;
     }
 
     // MODIFIES: this
     // EFFECTS: sets the completion status of the task to the given boolean value
     public void setCompletion(boolean isCompleted) {
-        
+        this.isCompleted = isCompleted;
     }
 
     // EFFECTS: returns the due date of this task
     @Override
     public Date getDueDate() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDueDate'");
+        return this.dueDate;
     }
 
     // EFFECTS: returns the weight of this task
     @Override
     public int getWeight() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getWeight'");
+        return this.weight;
     }
 
     // EFFECTS: returns whether this task has been marked as completed
     @Override
     public boolean isCompleted() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isCompleted'");
+        return this.isCompleted;
     }
     
 }
