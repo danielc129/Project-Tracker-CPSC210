09c3d63ecd9c00c4b15edb0e91cd2ef3367e60ad
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-07 17:34:47 -0800

    Implement Date class

--- src/main/model/Date.java
+++ src/main/model/Date.java
@@ -1,41 +1,88 @@
 package model;
 
+import model.exceptions.IllegalMonthException;
+
 // Represents a date with a day, month, and year
 public class Date {
+    private int day;
+    private int month;
+    private int year;
 
-    // REQUIRES: 1 <= month <= 12, day is valid for the given month (for example, 1 <= day <= 31 for month = 1)
+    // REQUIRES: day is valid for the given month (for example, 1 <= day <= 31 for month = 1)
     // EFFECTS: creates a date object with the given date of the month, the month, and the year
-    public Date(int day, int month, int year) {
-
+    //          if month is not between 1 and 12, throws IllegalMonthException
+    public Date(int day, int month, int year) throws IllegalMonthException {
+        if (month < 1 || month > 12) {
+            throw new IllegalMonthException();
+        }
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
 
     // EFFECTS: return the day of the month
     public int getDay() {
-        return -1;
+        return this.day;
     }
 
     // EFFECTS: return the month as an integer
     public int getMonth() {
-        return -1;
+        return this.month;
     }
 
     // EFFECTS: return the month in string form (for example, month = 1 would return "January")
     public String getMonthAsString() {
+        switch (month) {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9: 
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+        }
         return null;
     }
 
     // EFFECTS: returns a string in the format "getMonthAsString() day, year"
     public String getDateAsString() {
-        return null;
+        return getMonthAsString() + " " + this.day + ", " + this.year;
     }
 
     // EFFECTS: return the year
     public int getYear() {
-        return -1;
+        return this.year;
     }
 
     // EFFECTS: returns -1 if this is before other, 0 if this is the same date as other, and 1 if this is after other
     public int compareTo(Date other) {
-        return -1;
+        if (this.year < other.year) {
+            return -1;
+        } else if (this.year == other.year && this.month < other.month) {
+            return -1;
+        } else if (this.year == other.year && this.month == other.month && this.day < other.day) {
+            return -1;
+        } else if (this.year == other.year && this.month == other.month && this.day == other.day) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 }
--- src/test/model/DateTest.java
+++ src/test/model/DateTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.exceptions.IllegalMonthException;
+
 public class DateTest {
     private Date date1;
     private Date date2;
@@ -23,7 +25,7 @@ public class DateTest {
     private Date date15;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws IllegalMonthException {
         date1 = new Date(8, 1, 2025);
         date2 = new Date(12, 2, 2024);
         date3 = new Date(31, 3, 2025);
