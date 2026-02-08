a8cdbf1e1856f258612258443623dcba3d6961e5
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-07 18:07:28 -0800

    Specify Task abstract class

--- src/main/model/Task.java
+++ src/main/model/Task.java
@@ -4,4 +4,42 @@ package model;
 // Inter-class structure based on D4 (Composite Pattern) Lecture Lab
 public abstract class Task {
 
+    // EFFECTS: creates a task with the given name and description
+    public Task(String name, String description) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a subtask to this task with the given name, description, due date, and weight
+    public abstract void addSubtask(String name, String description, Date dueDate, int weight);
+
+
+    // MODIFIES: this
+    // EFFECTS: sets the task name to the given name
+    public void setName(String name) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the task description to the given description
+    public void setDescription(String description) {
+
+    }
+
+    // EFFECTS: returns the name of the task
+    public String getName() {
+        return null;
+    }
+
+    // EFFECTS: returns the description of the task
+    public String getDescription() {
+        return null;
+    }
+
+    // EFFECTS: returns the due date of the task
+    public abstract Date getDueDate();
+
+    // EFFECTS: returns the weight of the task
+    public abstract int getWeight();
+
 }
