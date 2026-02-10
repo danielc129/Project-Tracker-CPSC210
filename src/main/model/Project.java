45f80b0b8143169e9fcbdb63347d573b0dc349de
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-10 08:56:07 -0800

    Specify Project class

--- src/main/model/Project.java
+++ src/main/model/Project.java
@@ -1,5 +1,27 @@
 package model;
 
+// Represents a project 
 public class Project {
 
+    // EFFECTS: creates a new project with the given name and description 
+    public Project(String name, String description) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds the given task to the project
+    public void addTask(Task task) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes the given task from the project
+    public void removeTask(Task task) {
+
+    }
+
+    // EFFECTS: gets the percentage completion of the project (as an int from 0 to 100), taking into account task weights
+    public int getCompletionPercentage() {
+        return -1;
+    }
 }
