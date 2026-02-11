a88f3e8402e73ee1e164bb5dd10a07d4f0e22159
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-10 22:47:23 -0800

    Specify ProjectTrackerApp class

--- src/main/ui/ProjectTrackerApp.java
+++ src/main/ui/ProjectTrackerApp.java
@@ -1,8 +1,58 @@
 package ui;
 
 // Project tracker application
+// ATTRIBUTION: Based on Teller project 
 public class ProjectTrackerApp {
+
+    // EFFECTS: runs the project tracker application
     public ProjectTrackerApp() {
+        runProjectTracker();
+    }
+
+    // MODIFIES: this
+    // EFECTS: processes user input
+    private void runProjectTracker() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user command 
+    private void processCommand(String command) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes the project
+    private void init() {
+
+    }
+
+    // EFFECTS: displays menu of options to user
+    private void displayMenu() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a task to the currently selected task
+    private void addTask() {
+
+    }
+    
+    // MODIFIES: this
+    // EFFECTS: removes the currently selected task
+    private void removeTask() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: edits the currently selected task's details
+    private void editTask() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: marks the selected task as complete or incomplete
+    private void toggleCompletion() {
 
     }
 }
