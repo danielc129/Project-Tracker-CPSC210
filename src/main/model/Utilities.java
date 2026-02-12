4cb2408b93e5eb442ee8d7e7759aaa3838f0b07b
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-12 15:45:27 -0800

    Implement Utilities class

--- src/main/model/Utilities.java
+++ src/main/model/Utilities.java
@@ -1,18 +1,38 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // Provides common utility methods not suitable for addition to the Task class
 public class Utilities {
-
+    private final LeafTask REFERENCE_LEAF_TASK = new LeafTask("", "", new Date(1, 1, 1), 1);
 
     // EFFECTS: returns a new list of the given tasks sorted in order of closest to furthest due date
     public List<Task> sortTasks(List<Task> tasks) {
-        return null;
+        List<Task> sortedList = new ArrayList<>();
+        for (Task task : tasks) {
+            if (sortedList.isEmpty()) {
+                sortedList.add(task);
+            } else {
+                boolean added = false;
+                for (int i = sortedList.size() - 1; i >= 0; i--) {
+                    Task comparingTask = sortedList.get(i);
+                    if (task.getDueDate().compareTo(comparingTask.getDueDate()) >= 0) {
+                        sortedList.add(i + 1, task);
+                        added = true;
+                        break;
+                    } 
+                }
+                if (!added) {
+                    sortedList.add(0, task);
+                }      
+            }
+        }
+        return sortedList;
     }
 
     // EFFECTS: returns true if the given task is of actual type LeafTask
     public boolean isLeafTask(Task task) {
-        return false;
+        return task.getClass().getName().equals(REFERENCE_LEAF_TASK.getClass().getName());
     }
 }
