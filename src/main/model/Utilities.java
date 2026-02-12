1eddae738c71d8220f58ffc0d6d25ed151d127f3
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-12 09:06:05 -0800

    Specify Utilities class

---
+++ src/main/model/Utilities.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.List;
+
+// Provides common utility methods not suitable for addition to the Task class
+public class Utilities {
+
+
+    // EFFECTS: returns a new list of the given tasks sorted in order of closest to furthest due date
+    public List<Task> sortTasks(List<Task> tasks) {
+        return null;
+    }
+
+    // EFFECTS: returns true if the given task is of actual type LeafTask
+    public boolean isLeafTask(Task task) {
+        return false;
+    }
+}
