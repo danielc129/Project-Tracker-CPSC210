76f10f43d877dd7d0bf098795d3fcc6a564ae1af
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-20 14:22:43 -0800

    Implement ProjectList class

--- src/main/model/ProjectList.java
+++ src/main/model/ProjectList.java
@@ -1,29 +1,32 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectList {
+    private List<Project> projects;
 
     // EFFECTS: creates an empty project list
     public ProjectList() {
-
+        projects = new ArrayList<>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds the given project to the project list
     public void addProject(Project project) {
-
+        projects.add(project);
     }
     
     // EFFECTS: returns the projects added to the list
     public List<Project> getProjects() {
-        return null;
+        return projects;    
     }
 
     // REQUIRES: the given project is in the list
     // MODIFIES: this
     // EFFECTS: removes the given project from the project list
     public void removeProject(Project project) {
-
+        projects.remove(project);
     }
+    
 }
