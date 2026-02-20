fb8eda319a97ffa95e166f24543d43ef6588f892
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-20 14:12:14 -0800

    Specify ProjectList class

---
+++ src/main/model/ProjectList.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.List;
+
+public class ProjectList {
+
+    // EFFECTS: creates an empty project list
+    public ProjectList() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds the given project to the project list
+    public void addProject(Project project) {
+
+    }
+    
+    // EFFECTS: returns the projects added to the list
+    public List<Project> getProjects(Project project) {
+        return null;
+    }
+}
