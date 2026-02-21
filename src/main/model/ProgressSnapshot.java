6e4b36a5b9d189e98482534d2762e57b1ecf5d1d
Daniel Chen <dchen63@students.cs.ubc.ca>
2026-02-21 08:15:41 -0800

    Implement ProgressSnapshot

--- src/main/model/ProgressSnapshot.java
+++ src/main/model/ProgressSnapshot.java
@@ -4,20 +4,23 @@ import java.time.LocalDateTime;
 
 // Represents a snapshot of the completion percentage of a project at a specific time
 public class ProgressSnapshot {
+    private int completionPercentage;
+    private LocalDateTime time;
 
     // EFFECTS: creates a progress snapshot with the given completion percentage and time
     public ProgressSnapshot(int completionPercentage, LocalDateTime time) {
-
+        this.completionPercentage = completionPercentage;
+        this.time = time;
     }
 
     // EFFECTS: returns the completion percentage
     public int getCompletionPercentage() {
-        return -1;
+        return this.completionPercentage;
     }
 
     // EFFECTS: returns the time of the snapshot
     public LocalDateTime getTime() {
-        return null;
+        return this.time;
     }
-    
+
 }
